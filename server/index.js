3f1422514c7427564a0738d8b44112981e72c380
Mark Romano <markgromano31@gmail.com>
2018-03-16 10:05:46 -0400

    x

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const config = require('../webpack.config');
 const compiler = webpack(config);
 const app = express();
 
-app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-}));
-app.use(require('webpack-hot-middleware')(compiler));
+// app.use(require('webpack-dev-middleware')(compiler, {
+//     noInfo: true,
+//     publicPath: config.output.publicPath
+// }));
+// app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(express.static('public'));
 
