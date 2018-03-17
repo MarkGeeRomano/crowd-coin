0f70391f286ae7c61a69c44a4b3a25281f10f995
Mark Romano <markgromano31@gmail.com>
2018-03-17 16:33:03 -0400

    x

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     rules: [
